e950dc82d44b738cc7665f29e8ea3c96d740f7f8
lubanno7 <y.dragon.hu@aliyun.com>
2018-06-27 19:46:25 +0800

    修改版本

---
+++ index.ts
@@ -0,0 +1,6 @@
+export { default as Base } from './src/base/base';
+export { default as Aop } from './src/aop/aop';
+export { default as Data } from './src/data/data';
+export { default as Reflect } from './src/reflect/reflect';
+export { default as Remote } from './src/remote/remote';
+export { default as Security } from './src/security/security';
--- src/index.ts
+++
@@ -1,6 +0,0 @@
-export { default as Base } from './base/base';
-export { default as Aop } from './aop/aop';
-export { default as Data } from './data/data';
-export { default as Reflect } from './reflect/reflect';
-export { default as Remote } from './remote/remote';
-export { default as Security } from './security/security';
